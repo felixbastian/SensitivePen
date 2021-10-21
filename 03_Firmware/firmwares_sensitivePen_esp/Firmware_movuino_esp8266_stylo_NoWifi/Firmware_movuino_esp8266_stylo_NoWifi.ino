941201f5551c3f3f2636b7f7f3203866cede8788
libcraps <56928268+libcraps@users.noreply.github.com>
2021-10-21 09:20:50 +0200

    Update Firmware_movuino_esp8266_stylo_NoWifi.ino
    time out is in sec from movuino

--- 03_Firmware/firmwares_sensitivePen_esp/Firmware_movuino_esp8266_stylo_NoWifi/Firmware_movuino_esp8266_stylo_NoWifi.ino
+++ 03_Firmware/firmwares_sensitivePen_esp/Firmware_movuino_esp8266_stylo_NoWifi/Firmware_movuino_esp8266_stylo_NoWifi.ino
@@ -218,7 +218,7 @@ void loop() {
         //------- GET MPU DATA --------
         IMU.readSensor();
         get9axesDataMPU(IMU, &ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-        currentTime = (millis() - startTime)*0.001;
+        currentTime = millis() - startTime;
 
         writeInFileMovuinoData(file, sep);
         digitalWrite(pinLedBat, HIGH);
