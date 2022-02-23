12f3b5bd1048d3d31157ed7470afc5f4aeccb1fc
Chiara Palma <chiara.palma@student-cs.fr>
2022-02-23 14:56:43 +0100

    add tilt features

--- 06_Python/Features_extraction/Feature_ extraction_script/tiltFeatures.py
+++ 06_Python/Features_extraction/Feature_ extraction_script/tiltFeatures.py
@@ -1,23 +1,91 @@
 import pandas as pd
 import numpy as np
 
+#general function that computes all stats
+def add_stats(column):
+    mean = column.mean()
+    max = column.max()
+    min = column.min()
+    std = column.std()
+    var = column.var()
+    return mean, max, min, std, var
+
 #calculating mean, max, min SD of tilt-azimuth
 def tilt_azimuth(df):
-    print('test')
-    mean = 0
-    max = 0
-    min = 0
-    SD = 0
+    '''
+    Input: window
+    Output: mean, max, min, std and var of azimuth in the window
+    '''
+    azimuth = df["psi"]
+    features = {}
+    mean, max, min, std, var = add_stats(azimuth)
+    features["AZ_mean"] = [mean]
+    features["AZ_max"] = [max]
+    features["AZ_min"] = [min]
+    features["AZ_std"] = [std]
+    features["AZ_var"] = [var]
 
-    return mean, max, min, SD
+    return features
+
+def tilt_azimuth_change(df):
+    '''
+    Input: window
+    Output: mean, max, min, std and var of azimuthm change in the window
+    '''
+    azimuth_change = df["psi"].diff()
+    features = {}
+    mean, max, min, std, var = add_stats(azimuth_change)
+    features["AZ_diff_mean"] = [mean]
+    features["AZ_diff_max"] = [max]
+    features["AZ_diff_min"] = [min]
+    features["AZ_diff_std"] = [std]
+    features["AZ_diff_var"] = [var]
+
+    return features
+
+def tilt_altitude(df):
+    '''
+    Input: window
+    Output: mean, max, min, std and var of altitude in the window
+    '''
+    altitude = df["theta"]
+    features = {}
+    mean, max, min, std, var = add_stats(altitude)
+    features["AL_mean"] = [mean]
+    features["AL_max"] = [max]
+    features["AL_min"] = [min]
+    features["AL_std"] = [std]
+    features["AL_var"] = [var]
+
+    return features
+
+def tilt_altitude_change(df):
+    '''
+    Input: window
+    Output: mean, max, min, std and var of azimuthm change in the window
+    '''
+    altitude_change = df["theta"].diff()
+    features = {}
+    mean, max, min, std, var = add_stats(altitude_change)
+    features["AL_diff_mean"] = [mean]
+    features["AL_diff_max"] = [max]
+    features["AL_diff_min"] = [min]
+    features["AL_diff_std"] = [std]
+    features["AL_diff_var"] = [var]
+
+    return features
+
 
 def tiltFeatures(df):
 
-    #create tilt_azimuth features
-    mean_AZ, max_AZ, min_AZ, SD_AZ = tilt_azimuth(df)
+    #create features
+    features_AZ = tilt_azimuth(df)
+    features_AL = tilt_altitude(df)
+    features_AZ_diff = tilt_azimuth_change(df)
+    features_AL_diff = tilt_altitude_change(df)
 
     #add features to DataFrame
-    tiltDataFrame = pd.DataFrame([mean_AZ, max_AZ, min_AZ, SD_AZ])
+    all = [features_AZ,features_AL,features_AZ_diff,features_AL_diff]
+    tilt_df = pd.concat([pd.DataFrame.from_dict(i) for i in all], axis =1)
 
-
-    return tiltDataFrame
+    return tilt_df
