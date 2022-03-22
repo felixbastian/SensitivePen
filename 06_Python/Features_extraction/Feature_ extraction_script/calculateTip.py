4f6b566154770b587fa233069078d032b4b52800
felixbastian <felixbastian.hans@gmail.com>
2022-03-22 22:00:02 +0100

    prep for Ana

--- 06_Python/Features_extraction/Feature_ extraction_script/calculateTip.py
+++ 06_Python/Features_extraction/Feature_ extraction_script/calculateTip.py
@@ -10,18 +10,16 @@ def calculateTip(df):
     #When only looking at the translation, the tip and the top move the same in the coordinate system -> accX, accY, accZ
     #When only looking at the rotation of the pen, we have to account for relative location changes that are dependent on the length of the pen
 
-    translationAdjustment = [df['accX'], df['accY'], df['accZ']]
+
+    translationAdjustment = [df['accX_Top'], df['accY_Top'], df['accZ_Top']]
     rotationAdjustment = [2*(np.cos(df['psi'])*np.cos(df['theta'])-(np.sin(df['psi'])*np.sin(df['theta']))),
                         2*(np.sin(df['psi'])*np.cos(df['theta'])+(np.cos(df['psi'])*np.sin(df['theta']))),
                         np.sin(df['theta'])]
+
 
     #Looking at the translation and rotation together:
     #Formula = TranslationAdjusment + length*rotationAdjustment
     translatedDF = pd.DataFrame(np.add(lengthOfPen*np.array(rotationAdjustment),translationAdjustment))
     translatedDF = translatedDF.rename(index={0: "accX_Tip", 1: "accY_Tip", 2: "accZ_Tip"})
-
-    # translatedDF = translatedDF.append(df['psi'])
-    # translatedDF = translatedDF.append(df['theta'])
-
 
     return translatedDF.T
